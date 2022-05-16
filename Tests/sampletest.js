2afa7c43e51785992e9712b68ca90a55eb29ef0f
Dayalan Ganesan <dayalan.ganesan@home-in.com.au>
2022-05-16 19:27:10 +1000

    First Code

--- Tests/sampletest.js
+++ Tests/sampletest.js
@@ -3,7 +3,9 @@ const { By } = require('selenium-webdriver');
 const assert = require('assert');
 const env = require('../env.json');
 const screenObject = require('../ObjectRepository/ObjectRepo.json');
+
 // Input capabilities
+
 const capabilities = {
     'device': 'iPhone 11',
     'realMobile': 'true',
@@ -13,35 +15,44 @@ const capabilities = {
     'build': 'BStack Build Number 1' // CI/CD job or build name
 }
 
+
 async function runTestWithCaps() {
     let driver = new webdriver.Builder().withCapabilities({}).forBrowser("chrome").build();
     await driver.manage().window().maximize();
     try {
         await driver.get(env.url);
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.home.btnLogin, 10000))).click();
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtEmail, 10000))).sendKeys(env.username);
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.home.btnLogin, 20000))).click();
+        await driver.sleep(12000);
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtEmail, 50000))).sendKeys(env.username);
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.txtPassword, 10000))).sendKeys(env.password);
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.loginScreen.btnLogin, 10000))).click();
-        //switch to frame using web element
+       // switch to frame using web element
         await driver.sleep(15000);
-        //await driver.wait(webdriver.until.elementLocated(driver.findElement(screenObject.homeScreen.ifrHomeScreen, 20000)))
-        await driver.switchTo().frame(driver.findElement(screenObject.homeScreen.ifrHomeScreen));
+        await driver.findElement(screenObject.homeScreen.ifrHomeScreen);
+       // await driver.wait(webdriver.until.elementLocated(driver.findElement(screenObject.homeScreen.ifrHomeScreen, 70000)))
+        //await driver.switchTo().frame(driver.findElement(screenObject.homeScreen.ifrHomeScreen));
 
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 15000))).then(ele => {
+        /*await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 75000))).then(ele => {
             ele.click();
         }).catch(async(ele) => {
-
+        await driver.sleep(25000);    
             await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.homeScreen.btnaddaproperty, 2000))).click();
         })
-        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.txtPropertyAddress, 10000))).sendKeys("107 Esplanade , Rockingham WA 6168");
+       
+        await driver.sleep(25000);
+        await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.txtPropertyAddress, 10000))).sendKeys("105 Ellery Crescent, Acton ACT 2601");
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpSaleType, 10000))).sendKeys("Private Treaty");
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.drpContractofSale, 10000))).sendKeys("No");
         await driver.wait(webdriver.until.elementIsVisible(driver.findElement(screenObject.addPropertyScreen.btnGetStarted, 10000))).click();
-
-    } catch (e) {
+*/
+    } 
+    
+    catch (e) {
         //marking the test as Failed if product has not been added to the cart
         console.log("Error:", e.message)
     }
-    // await driver.quit();
+
+    
+     //await driver.quit();
 }
 runTestWithCaps();
